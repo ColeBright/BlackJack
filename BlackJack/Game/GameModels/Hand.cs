f7c2daf88cb423b36dea97ccb9b7da143dee12f3
Cole Bright <colebbright@gmail.com>
2025-11-13 17:39:54 -0600

    Added core logic and models (Card, Deck, Hand) and a test project to establish the guardrails for that logic. Also added card image assets.

---
+++ BlackJack/Game/GameModels/Hand.cs
@@ -0,0 +1,31 @@
+﻿namespace BlackJack.Game.GameModels
+{
+    public class Hand
+    {
+        private List<Card> cards = new List<Card>();
+
+        public bool IsDealer { get; }
+
+        public Hand(bool isDealer = false) {
+            IsDealer = isDealer;
+        }
+
+        public IReadOnlyList<Card> Cards => cards;
+        public void AddCard(Card card) => cards.Add(card);
+
+        public int GetValue()
+        {
+            int total = cards.Sum(c => c.GetValue());
+            int aces = cards.Count(c => c.Rank == Rank.Ace);
+
+            while(total > 21 && aces > 0)
+            {
+                total -= 10;
+                aces--;
+            }
+
+            return total;
+        }
+
+    }
+}
