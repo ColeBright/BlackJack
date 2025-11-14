53a39f4e0bab4d6e01ea587a711bc71a19bbfadd
Cole Bright <colebbright@gmail.com>
2025-11-14 17:08:29 -0600

    Added basic UI representation with a new GameController, Game view, and Card view partial

---
+++ BlackJack/Controllers/GameController.cs
@@ -0,0 +1,41 @@
+﻿using BlackJack.Game;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlackJack.Controllers
+{
+    public class GameController : Controller
+    {
+        private GameEngine engine;
+        public GameController() 
+        {
+             engine = new GameEngine();
+        }
+
+
+        public IActionResult Game()
+        {
+            return View("Game", engine);
+        }
+
+        public IActionResult Start()
+        {
+            var result = engine.StartRound();
+
+            return View("Game", engine);
+        }
+
+        public IActionResult Hit()
+        {
+            var result = engine.PlayerHit();
+
+            return View("Game", engine);
+        }
+
+        public IActionResult Stand()
+        {
+            var result = engine.PlayerStand();
+
+            return View("Game", engine);
+        }
+    }
+}
