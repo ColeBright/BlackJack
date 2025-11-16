74be7a3a3c6ade8ecd4ed0e4a6c12723c5f3f676
Cole Bright <colebbright@gmail.com>
2025-11-15 22:35:51 -0600

    Got rid of the UiState record and polished styles

--- BlackJack/Game/UiSession/UiState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlackJack.Game.UiSession
-{
-    public record UiState(
-        List<string> Deck,
-        List<string> PlayerCards,
-        List<string> DealerCards,
-        bool PlayerHasStood = false
-        );
-}
